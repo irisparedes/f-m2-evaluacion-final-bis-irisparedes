55b4e240806afb712e9f41f389b00f138a6de925
iris paredes <irisparedesjimenez@gmail.com>
2019-05-01 22:12:49 +0200

    add fetch and functions to paint cards when click the button

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,79 @@
 'use strict';
-
 console.log('>> Ready :)');
+const btn = document.querySelector('.btn');
+let arrData = null;
+let urlBackImage = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
+let list = document.querySelector('.list');
+//const arrRadio = document.querySelectorAll('.input');
+//const inputValue = input.value;
+//const url = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
+
+function getRadioValue () {
+  let radioValue = '';
+  const arrRadio = document.querySelectorAll('.input');
+  for (const radio of arrRadio) {
+    if (radio.checked) {
+      radioValue = radio.value;
+    }
+  }
+  return radioValue;
+}
+function createProperties (arrData) {
+  for (let i = 0; i < arrData.length; i++) {
+    let data = arrData[i];
+    data.id = i;
+    data.isFront = false;
+  }
+}
+
+function paintCards (arrCard) {
+  list.innerHTML = '';
+  for (const card of arrCard) {
+
+    const cardLi = document.createElement('li');
+    cardLi.classList.add('card');
+    cardLi.setAttribute('id', card.id);
+
+    const divFrontImage = document.createElement('div');
+    divFrontImage.classList.add('div__front-card');
+    const frontImage = document.createElement('img');
+    frontImage.classList.add('front__card');
+    //frontImage = card.image;
+    frontImage.setAttribute('src', card.image);
+
+
+
+    const divBackImage = document.createElement('div');
+    divBackImage.classList.add('back__card');
+    const backImage = document.createElement('img');
+    backImage.classList.add('back__card');
+    backImage.src = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';//urlBackImage;
+
+
+
+      divFrontImage.appendChild(frontImage);
+      divBackImage.appendChild(backImage);
+      cardLi.appendChild(divFrontImage);
+      cardLi.appendChild(divBackImage);
+      cardLi.appendChild(frontImage);
+      cardLi.appendChild(backImage);
+      list.appendChild(cardLi);
+
+
+
+  }
+}
+
+function numberCards () {
+  const inputValue = getRadioValue();
+  const url = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
+      fetch((url + inputValue + '.json'))
+        .then(response => response.json())
+        .then(arrData => {
+          createProperties(arrData);
+          paintCards(arrData);
+        });
+}
+
+btn.addEventListener('click', numberCards);
+
