dfe8dff94c902c1b326aad8621081df9887b7e1f
iris paredes <irisparedesjimenez@gmail.com>
2019-05-02 19:53:58 +0200

    add loscalStorage and function modifyCards, to show front

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,12 +1,18 @@
 'use strict';
-console.log('>> Ready :)');
 const btn = document.querySelector('.btn');
 let arrData = null;
-let urlBackImage = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 let list = document.querySelector('.list');
-//const arrRadio = document.querySelectorAll('.input');
-//const inputValue = input.value;
-//const url = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
+loadRadioValue();
+
+function loadRadioValue(){
+  let savedRadioValueLS = localStorage.getItem('radioValueLS');
+  let arrRadio = document.querySelectorAll('.input');
+  for (let radio of arrRadio) {
+    if (radio.value === savedRadioValueLS){
+      radio.checked = true;
+    }
+  }
+}
 
 function getRadioValue () {
   let radioValue = '';
@@ -16,6 +22,7 @@ function getRadioValue () {
       radioValue = radio.value;
     }
   }
+  localStorage.setItem('radioValueLS', radioValue);
   return radioValue;
 }
 function createProperties (arrData) {
@@ -34,51 +41,63 @@ function paintCards (arrCard) {
     cardLi.classList.add('card');
     cardLi.setAttribute('id', card.id);
 
+    cardLi.addEventListener('click', modifyCards);
+
     const divFrontImage = document.createElement('div');
     divFrontImage.classList.add('div__front-card');
 
     const divBackImage = document.createElement('div');
     divBackImage.classList.add('div__back__card');
 
-      if (card.isFront === true) {
-        const frontImage = document.createElement('img');
-        frontImage.classList.add('front__card');
-        //frontImage = card.image;
-        frontImage.setAttribute('src', card.image);
+    if (card.isFront === true) {
+      const frontImage = document.createElement('img');
+      frontImage.classList.add('front__card');
+      frontImage.setAttribute('src', card.image);
 
-        divFrontImage.appendChild(frontImage);
-        cardLi.appendChild(divFrontImage);
-        cardLi.appendChild(frontImage);
-      } else {
-        const backImage = document.createElement('img');
-        backImage.classList.add('back__card');
-        backImage.src = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';//urlBackImage;
+      divFrontImage.appendChild(frontImage);
+      cardLi.appendChild(divFrontImage);
+      cardLi.appendChild(frontImage);
+    } else {
+      const backImage = document.createElement('img');
+      backImage.classList.add('back__card');
+      backImage.src = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 
-        divBackImage.appendChild(backImage);
-        cardLi.appendChild(divBackImage);
-        cardLi.appendChild(backImage);
-      }
-      list.appendChild(cardLi);
+      divBackImage.appendChild(backImage);
+      cardLi.appendChild(divBackImage);
+      cardLi.appendChild(backImage);
+    }
+    list.appendChild(cardLi);
   }
 }
 
-
-// const turnAround = (event) => {
-//   let card = event.currentTarget;
-//   card.classList.toggle('');
-// }
+function modifyCards (event) {
+  let newCardLI = event.currentTarget;
+  arrData = JSON.parse(localStorage.getItem('arrDataLS'));
+  for (let i = 0; i < arrData.length; i++) {
+    let cardJSON = arrData[i];
+    if (parseInt(newCardLI.id) === cardJSON.id) {
+      if (cardJSON.isFront === true){
+        cardJSON.isFront = false;
+      }else{
+        cardJSON.isFront = true;
+      }
+    }
+  }
+  paintCards(arrData);
+  localStorage.setItem('arrDataLS', JSON.stringify(arrData));
+}
 
 function numberCards () {
   const inputValue = getRadioValue();
   const url = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
-      fetch((url + inputValue + '.json'))
-        .then(response => response.json())
-        .then(arrData => {
-          createProperties(arrData);
-          paintCards(arrData);
-        });
+  fetch((url + inputValue + '.json'))
+    .then(response => response.json())
+    .then(arrData => {
+      createProperties(arrData);
+      localStorage.setItem('arrDataLS', JSON.stringify(arrData));
+      paintCards(arrData);
+    });
 }
-
 btn.addEventListener('click', numberCards);
-//card.addEventListener('click', paintCards);
+
 
